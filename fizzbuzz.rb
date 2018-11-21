8f8709bef02d290ad4c3b0eaa49810587a256db9
Kat Maldon <katharinamaldon@gmail.com>
2018-11-21 15:24:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,9 @@
 def fizzbuzz(int)
   if (int % 3 == 0) 
     return "Fizz"
-  end 
-  if (int % 5 == 0) 
+  elsif (int % 5 == 0) 
     return "Buzz"
-  end
-  if (int % 15 == 0)
+  elsif (int % 15 == 0)
     return "FizzBuzz"
   end
 end
